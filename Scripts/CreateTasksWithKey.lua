3618a756aadd3dc5a5dd65a8023d69a7b31cfba3
AndyLu <laserlue@gmail.com>
2022-09-23 08:06:07 +0800

    update CreateTasksWithKey.lua, including pods number

--- Scripts/CreateTasksWithKey.lua
+++ Scripts/CreateTasksWithKey.lua
@@ -1,6 +1,12 @@
 local Json = require "json";
 print("2022_1111_Stress_Tests_In_K6: CreateTasksWithKey");
 
+POD = arg[1]
+if not (tonumber(POD)) then
+  print("First arguments shoulb be an number...");
+  return
+end
+
 local str = "";
 local file = io.open("./CreateTasksWithKeyENVs.json", "r");
 if not file then
@@ -15,16 +21,16 @@ end
 file:close();
 local ENVs = Json.decode(str);
 
--- e.g. k6 run --out json=todo/result_R10_D10_P10_M10.json --summary-export todo/summary_R10_D10_P10_M10.json ./CreateTasksWithKey.js --env RATE=10 --env DURATION=10 --env PREALLOCATEDVUS=10 --env MAXVUS=10
+-- e.g. k6 run --out json=path/result_R10_D10_P10_M10.json --summary-export path/summary_R10_D10_P10_M10.json ./CreateTasksWithKey.js --env RATE=10 --env DURATION=10 --env PREALLOCATEDVUS=10 --env MAXVUS=10
 local function GetCMD(v, i)
-  local path = "../Results/CreateTasksWithKey";
-  local outputFileName = path .. "/" .. i .. "_result_R" ..
-      v["RATE"] .. "_D" .. v["DURATION"] .. "_P" .. v["PREALLOCATEDVUS"] .. "_M" .. v["MAXVUS"] .. ".json";
-  local summaryReport = path .. "/" .. i .. "_summary_R" ..
-      v["RATE"] .. "_D" .. v["DURATION"] .. "_P" .. v["PREALLOCATEDVUS"] .. "_M" .. v["MAXVUS"] .. ".json";
+  local path = "../Results/CreateTasksWithKey_" .. POD .. "Pods";
+  local outputFileName = path .. "/" .. i .. "_result_" .. POD .. "Pods" ..
+      "_R" .. v["RATE"] .. "_D" .. v["DURATION"] .. "_P" .. v["PREALLOCATEDVUS"] .. "_M" .. v["MAXVUS"] .. ".json";
+  local summaryReportName = path .. "/" .. i .. "_summary_" .. POD .. "Pods" ..
+      "_R" .. v["RATE"] .. "_D" .. v["DURATION"] .. "_P" .. v["PREALLOCATEDVUS"] .. "_M" .. v["MAXVUS"] .. ".json";
   return "k6 run" ..
       " --out json=" .. outputFileName ..
-      " --summary-export " .. summaryReport ..
+      " --summary-export " .. summaryReportName ..
       " ./CreateTasksWithKey.js" ..
       " --env RATE=" .. v["RATE"] ..
       " --env DURATION=" .. v["DURATION"] ..
@@ -34,7 +40,7 @@ end
 
 -- os.execute("ulimit -n 655350")
 
-for key, value in pairs(ENVs) do
+for key, value in ipairs(ENVs) do
   for i = 1, 10, 1 do
     local cmd = GetCMD(value, i);
     print(cmd)
